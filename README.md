830571560b234657c64e994ea96c3a65dcd558ce
Tommybarral <isamutom27@hotmail.com>
2022-04-18 11:04:21 +0200

    starting project

---
+++ README.md
@@ -0,0 +1,7 @@
+# eshop_food_app
+
+E-shop Food app / magasin en ligne de nourriture.
+
+## packages used / paquets utilisés
+
+
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:eshop_food_app/screens/food_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FoodScreen(),
+    );
+  }
+}
